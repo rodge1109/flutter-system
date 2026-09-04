19d25c2988e6bd39c8af5f3fcedcc67bb79df786
rodge1109 <rodge.tonacao@gmail.com>
2026-09-05 01:06:45 +0800

    Fix Render build script and remove empty flutter file

--- render_build.sh
+++ render_build.sh
@@ -6,13 +6,13 @@ set -e
 echo "Starting Flutter web build for Render..."
 
 # 1. Download Flutter if it doesn't already exist in the build environment
-if [ ! -d "flutter" ]; then
+if [ ! -d "flutter_sdk" ]; then
   echo "Cloning Flutter stable channel..."
-  git clone https://github.com/flutter/flutter.git -b stable
+  git clone https://github.com/flutter/flutter.git -b stable flutter_sdk
 fi
 
 # 2. Add Flutter to the path
-export PATH="$PATH:`pwd`/flutter/bin"
+export PATH="$PATH:`pwd`/flutter_sdk/bin"
 
 # 3. Enable web support (just in case)
 flutter config --enable-web
